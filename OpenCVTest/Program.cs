c917f37faba5db6661b780101e771c6b28ad0005
Amadeus Lehmann <no@mail.com>
2024-02-22 20:09:16 +0100

    write results as images to disk

--- OpenCVTest/Program.cs
+++ OpenCVTest/Program.cs
@@ -7,23 +7,26 @@ using var testMat = new Mat();
 using var hsvImage = new Mat();
 using var mask = new Mat();
 using var dest = new Mat();
+using var maskColorMat = new Mat();
 using var cannyMat = new Mat();
 using var contourMat = new Mat();
 
 using var destWindow = new Window("Dest", WindowFlags.GuiExpanded);
 using var openedMask = new Window("Opening", WindowFlags.GuiExpanded);
 using var maskWindow = new Window("Mask", WindowFlags.GuiExpanded);
+using var maskColorWindow = new Window("MaskColor", WindowFlags.GuiExpanded);
 using var cannyWindow = new Window("Canny", WindowFlags.GuiExpanded);
 Cv2.CvtColor(src, hsvImage, ColorConversionCodes.BGR2HSV);
 var lowGreen = InputArray.Create([30, 0, 0]);
-var highGreen = InputArray.Create([90, 255, 255]);
+var highGreen = InputArray.Create([100, 255, 255]);
 Cv2.InRange(hsvImage, lowGreen, highGreen, mask);
 maskWindow.ShowImage(mask);
-Cv2.BitwiseAnd(src, src, dest, mask);
+Cv2.BitwiseAnd(src, src, maskColorMat, mask);
+maskColorWindow.ShowImage(maskColorMat);
+Cv2.CvtColor(maskColorMat, dest, ColorConversionCodes.BGR2GRAY);
 Cv2.Erode(dest, dest, null, null, 10);
 Cv2.Dilate(dest, dest, null, null, 10);
-Cv2.CvtColor(dest, dest, ColorConversionCodes.BGR2GRAY);
-Cv2.Threshold(dest, dest, 100, 255, ThresholdTypes.Binary);
+Cv2.Threshold(dest, dest, 50, 255, ThresholdTypes.Binary);
 openedMask.ShowImage(dest);
 Cv2.FindContours(dest, out var contours, OutputArray.Create(testMat), RetrievalModes.External, ContourApproximationModes.ApproxSimple);
 var areas = new List<double>();
@@ -38,4 +41,8 @@ Console.WriteLine(areas.Min());
 Console.WriteLine(areas[areas.Count / 2]);
 Console.WriteLine(JsonSerializer.Serialize(areas));
 destWindow.ShowImage(src);
+foreach (var window in new List<Window> { destWindow, cannyWindow, maskColorWindow, maskWindow, openedMask })
+{
+    Cv2.ImWrite(window.Name + ".png", window.Image);
+}
 Cv2.WaitKey();
