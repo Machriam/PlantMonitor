42a9a844df7200fa26ed2507200f33e4a62aacd3
NoUser <no@email.com>
2024-03-15 12:47:20 +0100

    added configuration to find ip addresses of devices

---
+++ GatewayApp/Backend/Plantmonitor.Server/Features/AppConfiguration/EnvironmentConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Plantmonitor.Server.Features.AppConfiguration
+{
+    public interface IEnvironmentConfiguration
+    {
+        string IpScanRange_From();
+        string IpScanRange_To();
+    }
+
+    public class EnvironmentConfiguration(IConfiguration configuration) : IEnvironmentConfiguration
+    {
+        public string IpScanRange_From() =>
+            configuration.GetConnectionString(nameof(IpScanRange_From)) ??
+                throw new Exception(nameof(IpScanRange_From) + " must be defined in appsettings.json");
+        public string IpScanRange_To() =>
+            configuration.GetConnectionString(nameof(IpScanRange_To)) ??
+                throw new Exception(nameof(IpScanRange_To) + " must be defined in appsettings.json");
+    }
+}
