4faecddf19209ce48fdc6c2c70d4d16c6b5e1479
Machriam <no@mail.com>
2024-03-20 14:30:16 +0100

    building of project in /srv and adding of systemd service

--- PlantMonitorControl/Install/install.sh
+++ PlantMonitorControl/Install/install.sh
@@ -5,3 +5,5 @@ curl -sSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --version lates
 dotnet= ~/.dotnet/dotnet 
 
 cd ~/PlantMonitor/PlantMonitorControl
+sudo dotnet build -c Release -o /srv/dist -r linux-arm --no-self-contained
+sudo ln -sf "$(pwd)"/.dotnet/dotnet /srv/dotnet
