2873f1684178716265f02fbb075a9a1da1a1b2f8
Machriam <no@mail.com>
2024-03-22 15:15:24 +0100

    sample movement

--- PlantMonitorControl/Features/MotorMovement/MotorMovementController.cs
+++ PlantMonitorControl/Features/MotorMovement/MotorMovementController.cs
@@ -9,16 +9,26 @@ namespace PlantMonitorControl.Features.MotorMovement;
 public class MotorMovementController(IEnvironmentConfiguration configuration) : ControllerBase
 {
     [HttpPost()]
-    public void MoveMotor(bool direction, bool enable, bool pulse)
+    public async Task MoveMotor()
     {
         using var controller = new GpioController(PinNumberingScheme.Board);
         var pinout = configuration.MotorPinout;
-        controller.OpenPin(pinout.Direction);
-        controller.OpenPin(pinout.Enable);
-        controller.OpenPin(pinout.Pulse);
+        controller.OpenPin(pinout.Direction, PinMode.Output);
+        controller.OpenPin(pinout.Enable, PinMode.Output);
+        controller.OpenPin(pinout.Pulse, PinMode.Output);
+        var left = PinValue.Low;
+        var right = PinValue.High;
+        var locked = PinValue.High;
+        var released = PinValue.Low;
 
-        controller.Write(pinout.Direction, direction ? PinValue.High : PinValue.Low);
-        controller.Write(pinout.Enable, enable ? PinValue.High : PinValue.Low);
-        controller.Write(pinout.Pulse, pulse ? PinValue.High : PinValue.Low);
+        controller.Write(pinout.Enable, locked);
+        controller.Write(pinout.Direction, left);
+        for (var i = 0; i < 10; i++)
+        {
+            controller.Write(pinout.Pulse, PinValue.High);
+            await Task.Delay(100);
+            controller.Write(pinout.Pulse, PinValue.Low);
+        }
+        controller.Write(pinout.Enable, released);
     }
 }
