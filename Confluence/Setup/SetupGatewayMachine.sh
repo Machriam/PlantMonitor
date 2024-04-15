c6b620c99ed79ddaa7122f5c284b162b7a2a3fde
Machriam <no@mail.com>
2024-04-15 13:53:26 +0200

    added commands to enable a wifi hotspot on debian bookworm. The default network-manager is uninstalled and a more compatible approach is used via hostapd, iptables and dnsmasq. The certificate does now use ips on 192.168.0 and 192.168.1 to be compatible between different networks.

--- Confluence/Setup/SetupGatewayMachine.sh
+++ Confluence/Setup/SetupGatewayMachine.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -14,18 +14,20 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 
 cd ~
-sudo apt install openssl
+# Create Self Signed Certificate for IP-Range 1-255
+sudo apt install -y openssl
 sudo mkdir /srv/secrets
 openssl req -newkey rsa:2048 -x509 -nodes -keyout ~/plantmonitor.key -new -out ~/plantmonitor.crt \
     -subj /CN=PlantMonitor/C=DE/ST=PM/L=PM/OU=Plantmonitor/O=Plantmonitor/emailAddress=plant@monitor.com/ -reqexts SAN -extensions SAN -config <(cat /etc/ssl/openssl.cnf \
-    <(printf '[SAN]\nsubjectAltName=IP:127.0.0.1, DNS:localhost, ') <(for i in {1..255}; do echo -n "IP:192.168.0.$i, "; done | sed 's/, $//')) -sha256 -days 3650 -addext basicConstraints=CA:true
+    <(printf '[SAN]\nsubjectAltName=IP:127.0.0.1, DNS:localhost, ') <(for i in {1..255}; do echo -n "IP:192.168.0.$i, IP:192.168.1.$i "; done | sed 's/, $//')) -sha256 -days 3650 -addext basicConstraints=CA:true
 sudo mv ~/plantmonitor.* /srv/secrets
-sudo apt-get install debconf
-sudo cp /srv/secrets/plantmonitor.crt /usr/share/ca-certificates/
-sudo dpkg-reconfigure ca-certificates # Gives a prompt to confirm adding of the new certificate
+# Trusting self signed certificate in Chrome
+sudo apt install -y libnss3-tools debconf
+certutil -d ~/.pki/nssdb/ -A -t "TC,," -n "PlantMonitor" -i /srv/secrets/plantmonitor.crt
+
 
 cd ../Dockerfiles
-sudo docker-compose -f docker-compose-linux.yml start
+sudo docker-compose up --detach
