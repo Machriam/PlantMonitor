c81847bccc0710afd6432e7b06c9b1d4c532ac13
Machriam <no@mail.com>
2024-05-15 14:33:39 +0200

    device must stream the actual byte data

--- PlantMonitorControl/Features/ImageTaking/FileStreamingReader.cs
+++ PlantMonitorControl/Features/ImageTaking/FileStreamingReader.cs
@@ -1,4 +1,6 @@
-﻿namespace PlantMonitorControl.Features.ImageTaking;
+﻿using Plantmonitor.Shared.Features.ImageStreaming;
+
+namespace PlantMonitorControl.Features.ImageTaking;
 
 public interface IFileStreamingReader
 {
@@ -12,6 +14,7 @@ public record struct FileInfo(DateTime CreationDate, int NewCounter, byte[]? Fil
 public class FileStreamingReader : IFileStreamingReader
 {
     public const string CounterFormat = "000000";
+    private static readonly string s_irEnding = CameraType.IR.Attribute<CameraTypeInfo>().FileEnding;
 
     public async Task<FileInfo> ReadNextFileWithSkipping(string imagePath, int counter, int howManyMoreRecentImagesMayExist, string fileEnding, CancellationToken token)
     {
@@ -21,10 +24,10 @@ public class FileStreamingReader : IFileStreamingReader
 
     public async Task<FileInfo> ReadNextFile(string imagePath, int counter, string fileEnding, CancellationToken token)
     {
-        var currentPath = Path.Combine(imagePath, $"{counter.ToString(CounterFormat)}{fileEnding}");
-        var nextPath = Path.Combine(imagePath, $"{(counter + 1).ToString(CounterFormat)}{fileEnding}");
+        var currentPath = Path.Combine(imagePath, counter.ToString(CounterFormat) + fileEnding);
+        var nextPath = Path.Combine(imagePath, (counter + 1).ToString(CounterFormat) + fileEnding);
         if (!Path.Exists(nextPath)) return new(default, counter, default);
-        var bytesToSend = await File.ReadAllBytesAsync(currentPath, token);
+        var bytesToSend = fileEnding == s_irEnding ? currentPath.GetBytesFromIrFilePath() : await File.ReadAllBytesAsync(currentPath, token);
         var creationTime = File.GetCreationTimeUtc(currentPath);
         File.Delete(currentPath);
         return new(creationTime, counter + 1, bytesToSend);
@@ -32,12 +35,12 @@ public class FileStreamingReader : IFileStreamingReader
 
     private static int SkipFiles(string imagePath, int counter, int skipCounter, string fileEnding)
     {
-        var outOfSyncPath = Path.Combine(imagePath, $"{(counter + skipCounter).ToString(CounterFormat)}{fileEnding}");
+        var outOfSyncPath = Path.Combine(imagePath, (counter + skipCounter).ToString(CounterFormat) + fileEnding);
         if (Path.Exists(outOfSyncPath))
         {
             for (var i = 0; i < skipCounter; i++)
             {
-                var deletePath = Path.Combine(imagePath, $"{(counter + i).ToString(CounterFormat)}{fileEnding}");
+                var deletePath = Path.Combine(imagePath, (counter + i).ToString(CounterFormat) + fileEnding);
                 if (Path.Exists(deletePath)) File.Delete(deletePath);
             }
             counter += skipCounter;
