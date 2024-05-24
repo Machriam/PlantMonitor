87b7e56e1d9cd8cb3080705d7ac30356f62c6f98
Machriam <no@mail.com>
2024-05-24 12:59:06 +0200

    Compiled a custom build of opencv.js to include the applyColorMap function. Added a custom opencv function class to serve those optional functions. The temperature readings are subtracted by 15°C and then divided by 10. This yields a range of 0-255, where 0 equals 15°C and 255 40.5°C with .1 steps. temperature readings below or above that are clipped.

--- GatewayApp/Frontend/Plantmonitor.Website/src/types/typeExtensions.d.ts
+++ GatewayApp/Frontend/Plantmonitor.Website/src/types/typeExtensions.d.ts
@@ -1,77 +1,77 @@
 
 interface String {
-	isEmpty(): boolean;
-	asBase64(): string;
-	fromBase64(): string;
-	urlEncoded(): string;
-	base64ToByteArray(): Uint8Array;
+    isEmpty(): boolean;
+    asBase64(): string;
+    fromBase64(): string;
+    urlEncoded(): string;
+    base64ToByteArray(): Uint8Array;
 }
 interface Array<T> {
-	mean(selector: (x: T) => number): number;
+    mean(selector: (x: T) => number): number;
 }
 interface BigInt {
-	fromTicksToDate(): Date;
+    fromTicksToDate(): Date;
 }
 interface Number {
-	roundTo(decimalPlaces: number): number;
-	kelvinToCelsius(): number;
-	isSuccessStatusCode(): boolean;
+    roundTo(decimalPlaces: number): number;
+    kelvinToCelsius(): number;
+    isSuccessStatusCode(): boolean;
 }
 interface Blob {
-	asBase64Url(): Promise<string>;
+    asBase64Url(): Promise<string>;
 }
 interface Uint8Array {
-	toInt32(): number;
-	toInt64(): bigint;
+    toInt32(): number;
+    toInt64(): bigint;
 }
 
 Uint8Array.prototype.toInt32 = function (this: Uint8Array): number {
-	return new DataView(this.slice(0, 4).buffer).getInt32(0, true);
+    return new DataView(this.slice(0, 4).buffer).getInt32(0, true);
 }
 
 Uint8Array.prototype.toInt64 = function (this: Uint8Array): bigint {
-	return new DataView(this.slice(0, 8).buffer).getBigInt64(0, true);
+    return new DataView(this.slice(0, 8).buffer).getBigInt64(0, true);
 }
 
 Array.prototype.mean = function (this: Array<T>, selector: (x: T) => number) {
-	if (this.length == 0) return 0;
-	return this.reduce((a, x) => a += selector(x), 0) / this.length;
+    if (this.length == 0) return 0;
+    return this.reduce((a, x) => a += selector(x), 0) / this.length;
 }
 Number.prototype.kelvinToCelsius = function (this: number): number {
-	return (this - 27315) / 100;
+    return (this - 27315) / 100;
 }
 
 Number.prototype.roundTo = function (this: number, decimalPlaces: number): number {
-	return +this.toFixed(decimalPlaces);
+    return +this.toFixed(decimalPlaces);
 }
 BigInt.prototype.fromTicksToDate = function (this: bigint): Date {
-	const zeroTime = BigInt(new Date("0001-01-01T00:00:00Z").getTime());
-	const tickDivider = BigInt(10000);
-	const milliseconds = Number(this / tickDivider + zeroTime);
-	return new Date(milliseconds);
+    const zeroTime = BigInt(new Date("0001-01-01T00:00:00Z").getTime());
+    const tickDivider = BigInt(10000);
+    const milliseconds = Number(this / tickDivider + zeroTime);
+    return new Date(milliseconds);
 }
 Number.prototype.isSuccessStatusCode = function (this: number): boolean {
-	return this == 200 || this == 204;
+    return this == 200 || this == 204;
 }
 String.prototype.isEmpty = function (this: string): boolean {
-	return this === undefined || this === null || this.length == 0;
+    return this === undefined || this === null || this.length == 0;
 }
 String.prototype.asBase64 = function (this: string): string {
-	return btoa(this);
+    return btoa(this);
 }
 String.prototype.fromBase64 = function (this: string): string {
-	return atob(this);
+    return atob(this);
 }
 String.prototype.base64ToByteArray = function (this: string): Uint8Array {
-	return Uint8Array.from(atob(this), c => c.charCodeAt(0));
+    return Uint8Array.from(atob(this), c => c.charCodeAt(0));
 }
 String.prototype.urlEncoded = function (this: string): string {
-	return encodeURIComponent(this);
+    return encodeURIComponent(this);
 }
 Blob.prototype.asBase64Url = async function (this: Blob): Promise<string> {
-	return new Promise((resolve) => {
-		const reader = new FileReader();
-		reader.onloadend = () => resolve(reader.result);
-		reader.readAsDataURL(this);
-	});
+    return new Promise((resolve) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result);
+        reader.readAsDataURL(this);
+    });
 }
