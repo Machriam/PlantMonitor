3d12aa379226a7f0227455153ddb1266170fc109
Machriam <no@mail.com>
2024-05-29 14:51:23 +0200

    added sample to start a measurement - which is currently not working

--- GatewayApp/Frontend/Plantmonitor.Website/src/features/temperatureMonitoring/+page.svelte
+++ GatewayApp/Frontend/Plantmonitor.Website/src/features/temperatureMonitoring/+page.svelte
@@ -1,11 +1,35 @@
 <script lang="ts">
     import {DeviceStreaming} from "~/services/DeviceStreaming";
     import {selectedDevice} from "../store";
-    import {TemperatureClient} from "~/services/GatewayAppApi";
+    import {
+        MeasurementDevice,
+        MeasurementStartInfo,
+        TemperatureClient,
+        TemperatureMeasurement,
+        type IMeasurementStartInfo
+    } from "~/services/GatewayAppApi";
     import {onDestroy} from "svelte";
     import {HubConnection} from "@microsoft/signalr";
     let _connection: HubConnection | undefined;
+    let _temperatureMeasurements: TemperatureMeasurement[] = [];
+    let _devices: MeasurementDevice[] = [];
     onDestroy(() => {});
+    async function getMeasurements() {
+        const temperatureClient = new TemperatureClient();
+        _temperatureMeasurements = await temperatureClient.measurements();
+    }
+    async function startMeasurement() {
+        if ($selectedDevice == undefined || $selectedDevice == null) return;
+        const temperatureClient = new TemperatureClient();
+        temperatureClient.addMeasurement(new MeasurementStartInfo({devices: _devices, ip: $selectedDevice.ip}));
+    }
+    async function getDevices() {
+        if ($selectedDevice == undefined || $selectedDevice == null) return;
+        const temperatureClient = new TemperatureClient();
+        _devices = (await temperatureClient.getDevices($selectedDevice.ip)).map(
+            (x) => new MeasurementDevice({deviceId: x, comment: ""})
+        );
+    }
     async function getTemperatures() {
         if ($selectedDevice == undefined || $selectedDevice == null) return;
         const temperatureClient = new TemperatureClient();
@@ -22,4 +46,12 @@
 <div class="col-md-12 row">
     <button on:click={getTemperatures} class=" btn btn-primary">Show Temperatures</button>
     <div>{$selectedDevice?.ip}</div>
+    <button on:click={getDevices} class="btn btn-primary">Get Devices</button>
+    <div class="col-md-12">
+        {#each _devices as device}
+            <div>{device.deviceId}</div>
+            <input type="text" class="form-control" bind:value={device.comment} />
+        {/each}
+    </div>
+    <button on:click={startMeasurement} class="btn btn-primary">Start Measurement</button>
 </div>
