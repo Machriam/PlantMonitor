d273f8ca0bab00d60d43d911103b9e0fd0eabb74
Machriam <no@mail.com>
2024-06-25 16:15:05 +0200

    added a manual stitching method to test getting a proper homography between two images. The implementation is based upon https://stackoverflow.com/questions/53079167/emgucv-opencv-orbdetector-finding-only-bad-matches It works much better than all of my manual implementations, see comparison between Good and Bad Feature Matching images. Major takeaways are: Request many features from ORB - not just the default amount. Lowes algorithm is implemented in Features2DToolbox. KNN matchings needs to have the model descriptors added before calling KnnMatch. http://6.869.csail.mit.edu/fa12/lectures/lecture13ransac/lecture13ransac.pdf is a good description of how feature mapping works.

--- GatewayApp/Backend/Plantmonitor.Server.Tests/Features/ImageStitching/PhotoStitcherTests.cs
+++ GatewayApp/Backend/Plantmonitor.Server.Tests/Features/ImageStitching/PhotoStitcherTests.cs
@@ -2,11 +2,12 @@
 using Plantmonitor.Server.Features.DeviceConfiguration;
 using Plantmonitor.Server.Features.ImageStitching;
 using System;
+using System.Diagnostics;
 using Xunit;
 
 namespace Plantmonitor.Server.Tests.Features.ImageStitching
 {
-    public class PhotoStitcherTests
+    public class PhotoStitcherTests : IDisposable
     {
         public PhotoStitcherTests()
         {
@@ -24,6 +25,23 @@ namespace Plantmonitor.Server.Tests.Features.ImageStitching
             var sut = CreatePhotoStitcher();
             var pictureFolder = $"{applicationPath}/PlantMonitorControl.Tests/TestData/HomographyTest";
             sut.StitchPhotos(pictureFolder);
+        }
+
+        [Fact]
+        public void StitchPhotosManual_StateUnderTest_ExpectedBehavior()
+        {
+            var applicationPath = Directory.GetCurrentDirectory().GetApplicationRootGitPath();
+            var sut = CreatePhotoStitcher();
+            var pictureFolder = $"{applicationPath}/PlantMonitorControl.Tests/TestData/HomographyTest";
+            sut.StitchPhotosManual(pictureFolder);
+        }
+
+        public void Dispose()
+        {
+            foreach (var process in Process.GetProcessesByName("testhost"))
+            {
+                process.Kill();
+            }
         }
     }
 }
--- GatewayApp/Backend/Plantmonitor.Server/Features/ImageStitching/PhotoStitcher.cs
+++ GatewayApp/Backend/Plantmonitor.Server/Features/ImageStitching/PhotoStitcher.cs
@@ -1,6 +1,9 @@
-﻿using Emgu.CV;
+﻿using System.Drawing;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
 using Emgu.CV.Features2D;
 using Emgu.CV.Stitching;
+using Emgu.CV.Structure;
 using Emgu.CV.Util;
 
 namespace Plantmonitor.Server.Features.ImageStitching;
@@ -29,82 +32,82 @@ public class PhotoStitcher
         stitcher.Dispose();
     }
 
-    /*
-    Features Finder: Utilizes ORB (Oriented FAST and Rotated BRIEF) for feature detection. The number of features to find can be adjusted.
-Features Matcher: Uses an affine matcher with specific parameters to match features between images.
-Estimator: Employs a homography-based estimator for initial camera estimation.
-Bundle Adjuster: Adjusts the camera parameters globally using the Ray method.
-Warper: Applies a spherical warping to the images.
-Exposure Compensator: Compensates for exposure differences using the Gain Blocks method.
-Seam Finder: Finds the optimal seams with the DpSeamFinder using color gradients as the cost function.
-Blender: Blends images together using a multi-band blender for seamless stitching.
-     * */
+    private static void FindMatch(Mat modelImage, Mat observedImage, out VectorOfKeyPoint modelKeyPoints,
+        out VectorOfKeyPoint observedKeyPoints, VectorOfVectorOfDMatch matches, out Mat mask, out Mat? homography)
+    {
+        const int K = 2;
+        const double UniquenessThreshold = 0.80d;
+        homography = null;
+        modelKeyPoints = new VectorOfKeyPoint();
+        observedKeyPoints = new VectorOfKeyPoint();
+        using var uModelImage = modelImage.GetUMat(AccessType.Read);
+        using var uObservedImage = observedImage.GetUMat(AccessType.Read);
+        var featureDetector = new ORB(9000);
+        var modelDescriptors = new Mat();
+        featureDetector.DetectAndCompute(uModelImage, null, modelKeyPoints, modelDescriptors, false);
+        var observedDescriptors = new Mat();
+        featureDetector.DetectAndCompute(uObservedImage, null, observedKeyPoints, observedDescriptors, false);
+        using var matcher = new BFMatcher(DistanceType.Hamming, false);
+        matcher.Add(modelDescriptors);
 
-    public void StitchPhotos2(string folderName)
+        matcher.KnnMatch(observedDescriptors, matches, K, null);
+        mask = new Mat(matches.Size, 1, DepthType.Cv8U, 1);
+        mask.SetTo(new MCvScalar(255));
+        Features2DToolbox.VoteForUniqueness(matches, UniquenessThreshold, mask);
+
+        var nonZeroCount = CvInvoke.CountNonZero(mask);
+        if (nonZeroCount >= 4)
+        {
+            nonZeroCount = Features2DToolbox.VoteForSizeAndOrientation(modelKeyPoints, observedKeyPoints,
+                matches, mask, 1.5, 20);
+            if (nonZeroCount >= 4)
+                homography = Features2DToolbox.GetHomographyMatrixFromMatchedFeatures(modelKeyPoints,
+                    observedKeyPoints, matches, mask, 2);
+        }
+    }
+
+    public static Mat Draw(Mat modelImage, Mat observedImage)
+    {
+        using var matches = new VectorOfVectorOfDMatch();
+        FindMatch(modelImage, observedImage, out var modelKeyPoints, out var observedKeyPoints, matches, out var mask, out var homography);
+        var result = new Mat();
+        Features2DToolbox.DrawMatches(modelImage, modelKeyPoints, observedImage, observedKeyPoints,
+            matches, result, new MCvScalar(255, 0, 0), new MCvScalar(0, 0, 255), mask);
+
+        if (homography != null)
+        {
+            var imgWarped = new Mat();
+            CvInvoke.WarpPerspective(observedImage, imgWarped, homography, modelImage.Size, Inter.Linear, Warp.InverseMap);
+            var rect = new Rectangle(Point.Empty, modelImage.Size);
+            var pts = new PointF[]
+            {
+                  new(rect.Left, rect.Bottom),
+                  new(rect.Right, rect.Bottom),
+                  new(rect.Right, rect.Top),
+                  new(rect.Left, rect.Top)
+            };
+
+            pts = CvInvoke.PerspectiveTransform(pts, homography);
+            var points = new Point[pts.Length];
+            for (var i = 0; i < points.Length; i++) points[i] = Point.Round(pts[i]);
+
+            using var vp = new VectorOfPoint(points);
+            CvInvoke.Polylines(result, vp, true, new MCvScalar(255, 0, 0, 255), 5);
+        }
+        CvInvoke.Resize(result, result, default, 0.5, 0.5);
+        return result;
+    }
+
+    public void StitchPhotosManual(string folderName)
     {
         var files = Directory.GetFiles(folderName).Take(2);
         var imagesToStitch = files.OrderBy(f => f).Select(f => new Mat(f)).ToList();
-
-        // Features Finder
-        var featuresFinder = new ORB();
-
-        // Features Matcher
-        var featuresMatcher = new AffineBestOf2NearestMatcher(false, false, 0.3f);
-
-        // Estimator
-        var estimator = new HomographyBasedEstimator(true);
-
-        // Bundle Adjuster
-        var adjuster = new BundleAdjusterRay();
-
-        // Warper
-        var warper = new SphericalWarper();
-
-        // Exposure Compensator
-        var compensator = new NoExposureCompensator();
-
-        // Seam Finder
-        var seamFinder = new DpSeamFinder(DpSeamFinder.CostFunction.ColorGrad);
-
-        // Blender
-        var blender = new MultiBandBlender();
-
-        // Stitching Pipeline
-        var stitcher = new Stitcher(Stitcher.Mode.Scans);
-        stitcher.SetFeaturesFinder(featuresFinder);
-        stitcher.SetFeaturesMatcher(featuresMatcher);
-        stitcher.SetEstimator(estimator);
-        stitcher.SetBundleAdjusterCompensator(adjuster);
-        stitcher.SetWarper(warper);
-        stitcher.SetExposureCompensator(compensator);
-        stitcher.SetSeamFinder(seamFinder);
-        stitcher.SetBlender(blender);
-
-        //using (var result = new Mat())
-        //{
-        //    var status = stitcher.Stitch(imagesToStitch, result);
-
-        //    if (status == Stitcher.Status.Ok)
-        //    {
-        //        result.Save(Path.Combine(folderName, "../result.png"));
-        //        Console.WriteLine("Stitching completed successfully.");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine($"Stitching failed: {status}");
-        //    }
-        //}
-
-        // Cleanup
-        foreach (var image in imagesToStitch) image.Dispose();
-        featuresFinder.Dispose();
-        featuresMatcher.Dispose();
-        estimator.Dispose();
-        adjuster.Dispose();
-        warper.Dispose();
-        compensator.Dispose();
-        seamFinder.Dispose();
-        blender.Dispose();
-        stitcher.Dispose();
+        var matches = new VectorOfVectorOfDMatch();
+        FindMatch(imagesToStitch[0], imagesToStitch[1], out var keyPoints, out var keyPoints2, matches, out var mask, out var homography);
+        var result = new Mat();
+        CvInvoke.WarpPerspective(imagesToStitch[1], result, homography, imagesToStitch[0].Size, Inter.Linear, Warp.InverseMap);
+        CvInvoke.Imshow("result", result);
+        CvInvoke.Imshow("test", Draw(imagesToStitch[0], imagesToStitch[1]));
+        CvInvoke.WaitKey();
     }
 }
