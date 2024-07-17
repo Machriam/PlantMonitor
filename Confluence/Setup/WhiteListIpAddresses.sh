cde6ad828ef9de571bd18b3106b065f2703e91ce
Machriam <no@mail.com>
2024-07-17 14:58:03 +0200

    added external webssh port to white listing aswell

--- Confluence/Setup/WhiteListIpAddresses.sh
+++ Confluence/Setup/WhiteListIpAddresses.sh
@@ -11,12 +11,14 @@ sudo iptables -P INPUT ACCEPT
 for ip in $ips; do
   sudo iptables -A INPUT -p tcp --dport 22 --source "$ip" -j ACCEPT
   sudo iptables -A INPUT -p tcp --dport 443 --source "$ip" -j ACCEPT
+  sudo iptables -A INPUT -p tcp --dport 450 --source "$ip" -j ACCEPT
   sudo iptables -A INPUT -p tcp --dport 5432 --source "$ip" -j ACCEPT
   sudo iptables -A INPUT -p tcp --dport 3389 --source "$ip" -j ACCEPT
 done
 
 sudo iptables -A INPUT -p tcp --dport 22 -j DROP
 sudo iptables -A INPUT -p tcp --dport 443 -j DROP
+sudo iptables -A INPUT -p tcp --dport 450 -j DROP
 sudo iptables -A INPUT -p tcp --dport 5432 -j DROP
 sudo iptables -A INPUT -p tcp --dport 3389 -j DROP
 
