3fd70a7a80776baf55cd019fb5ce3b6d49273fd8
Machriam <no@mail.com>
2024-07-24 16:57:27 +0200

    cropping works now properly with the local test image - but only as a rectangle. It should be a proper polygon crop in the end

--- GatewayApp/Backend/Plantmonitor.Server/Features/ImageStitching/ImageCropper.cs
+++ GatewayApp/Backend/Plantmonitor.Server/Features/ImageStitching/ImageCropper.cs
@@ -12,11 +12,12 @@ public class ImageCropper
         var minX = polygon.Min(p => p.X);
         var minY = polygon.Min(p => p.Y);
         var width = polygon.Max(p => p.X) - minX;
-        var height = minY - polygon.Max(p => p.Y);
+        var height = polygon.Max(p => p.Y) - minY;
         var roi = new Rectangle((int)minX, (int)minY, (int)width, (int)height);
         var crop = new Mat(image, roi);
-        CvInvoke.Imshow("bla", image);
+        CvInvoke.Imshow("bla", crop);
         CvInvoke.WaitKey(1000);
         image.Dispose();
+        crop.Dispose();
     }
 }
