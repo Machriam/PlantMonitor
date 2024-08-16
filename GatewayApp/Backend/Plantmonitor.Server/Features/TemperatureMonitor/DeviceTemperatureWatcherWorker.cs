46dea46216f54a1fc362b4d8af39278ee66dc9d4
Machriam <no@mail.com>
2024-08-16 12:21:00 +0200

    added logging to checking of temperature devices

--- GatewayApp/Backend/Plantmonitor.Server/Features/TemperatureMonitor/DeviceTemperatureWatcherWorker.cs
+++ GatewayApp/Backend/Plantmonitor.Server/Features/TemperatureMonitor/DeviceTemperatureWatcherWorker.cs
@@ -3,13 +3,13 @@ using Plantmonitor.Server.Features.AutomaticPhotoTour;
 
 namespace Plantmonitor.Server.Features.TemperatureMonitor;
 
-public class DeviceTemperatureWatcherWorker(IServiceScopeFactory scopeFactory) : IHostedService
+public class DeviceTemperatureWatcherWorker(IServiceScopeFactory scopeFactory, ILogger<DeviceTemperatureWatcherWorker> logger) : IHostedService
 {
     private Timer? _timer;
 
     public Task StartAsync(CancellationToken cancellationToken)
     {
-        _timer = new Timer(async _ => await RestartMeasurements(), default, 0, (int)TimeSpan.FromSeconds(5).TotalMilliseconds);
+        _timer = new Timer(async _ => await RestartMeasurements(), default, 0, (int)TimeSpan.FromSeconds(10).TotalMilliseconds);
         return Task.CompletedTask;
     }
 
@@ -23,11 +23,14 @@ public class DeviceTemperatureWatcherWorker(IServiceScopeFactory scopeFactory) :
         var expectedMeasurements = context.TemperatureMeasurements
             .Where(tm => !tm.Finished)
             .ToDictionary(tm => tm.Id);
+        logger.LogInformation("Checking Temperature devices");
         foreach (var measurement in measurements)
         {
             var correctRunningMeasurements = measurement.Select(m => expectedMeasurements.TryGetValue(m.MeasurementId, out var value) ? value : null);
+            logger.LogInformation("Checking Temperature measurement {measurement}", measurement.Key);
             if (correctRunningMeasurements.Any(rm => rm == null))
             {
+                logger.LogInformation("Stopping Temperature measurement {measurement}", measurement.Key);
                 temperatureWorker.StopMeasurement(measurement.Key);
                 continue;
             }
@@ -37,13 +40,16 @@ public class DeviceTemperatureWatcherWorker(IServiceScopeFactory scopeFactory) :
                     .OrderByDescending(tmv => tmv.Timestamp)
                     .FirstOrDefault(tmv => tmv.MeasurementFk == id);
                 var data = correctRunningMeasurements.First()!;
+                logger.LogInformation("Last Temperature received {timestamp} {temperature} {device}", lastTemperature?.Timestamp, lastTemperature?.Temperature, data.DeviceId);
                 if (lastTemperature != null && (lastTemperature.Timestamp - DateTime.UtcNow).TotalMinutes > 1)
                 {
+                    logger.LogWarning("Restart Temperature device {device}", data.DeviceId);
                     await restarter.RestartDevice(data.DeviceId.ToString(), data.PhotoTourFk, data.Comment);
                     break;
                 }
                 if (lastTemperature == null && (data.StartTime - DateTime.UtcNow).TotalMinutes > 1)
                 {
+                    logger.LogWarning("Restart Temperature device {device}", data.DeviceId);
                     await restarter.RestartDevice(data.DeviceId.ToString(), data.PhotoTourFk, data.Comment);
                     break;
                 }
@@ -51,11 +57,14 @@ public class DeviceTemperatureWatcherWorker(IServiceScopeFactory scopeFactory) :
         }
         foreach (var measurement in expectedMeasurements.Select(em => em.Value).GroupBy(em => em.DeviceId))
         {
+            logger.LogInformation("Checking expected measurement of {device}", measurement.Key);
             if (measurement.Any(m => m.IsThermalCamera())) continue;
             var okMeasurements = measurements.SelectMany(m => m.Select(x => x.MeasurementId)).ToHashSet();
             if (measurement.All(m => okMeasurements.Contains(m.Id))) continue;
+            logger.LogInformation("Resume measurement of {device}", measurement.Key);
             await temperatureWorker.ResumeMeasurement(measurement);
         }
+        logger.LogInformation("Temperature device checking finished");
         await Task.CompletedTask;
     }
 
