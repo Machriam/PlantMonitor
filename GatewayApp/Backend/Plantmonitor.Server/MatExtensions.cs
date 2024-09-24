83adc7c3c0ed0b17291461c83e58777e9dd2cf2b
Amadeus Lehmann <No@mail.com>
2024-09-24 22:56:10 +0200

    first steps to make a designated managed mat class

--- GatewayApp/Backend/Plantmonitor.Server/MatExtensions.cs
+++ GatewayApp/Backend/Plantmonitor.Server/MatExtensions.cs
@@ -5,9 +5,17 @@ using Serilog;
 
 namespace Plantmonitor.Server;
 
-internal static class MatExtensions
+public class ManagedMat(Mat mat) : IMat
 {
-    private static void LogCall(string memberName, string sourceFilePath, int sourceLineNumber, params Mat[] mats)
+    private bool _disposed = false;
+    private Guid _guid = Guid.NewGuid();
+    private Mat _mat = mat;
+    public void Dispose()
+    {
+        _disposed = true;
+        _mat.Dispose();
+    }
+    private void LogCall(string memberName, string sourceFilePath, int sourceLineNumber, params Mat[] mats)
     {
         //var sizes = mats.Select(m => $"{m.Width}x{m.Height}").Concat(", ");
         //var pointer = mats.Select(m => m.DataPointer).Concat(", ");
@@ -15,42 +23,54 @@ internal static class MatExtensions
         //Log.Logger.Debug($"Mat sizes: {sizes}, Pointer: {pointer}");
     }
 
-    public static T LogCall<T>(this Mat mat, Func<Mat, T> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    public T LogCall<T>(this Mat mat, Func<Mat, T> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
     {
         LogCall(memberName, sourceFilePath, sourceLineNumber, mat);
         return func(mat);
     }
 
-    public static void LogCall(this Mat mat, Action<Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    public void LogCall(this Mat mat, Action<Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
     {
         LogCall(memberName, sourceFilePath, sourceLineNumber, mat);
         func(mat);
     }
 
-    public static void LogCall(this Mat mat, Mat mat2, Action<Mat, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    public void LogCall(this Mat mat, Mat mat2, Action<Mat, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
     {
         LogCall(memberName, sourceFilePath, sourceLineNumber, mat, mat2);
         func(mat, mat2);
     }
 
-    public static void LogCall(this Mat mat, Mat mat2, Mat mat3, Action<Mat, Mat, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    public void LogCall(IMat mat2, IMat mat3, Action<Mat, Mat, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
     {
         LogCall(memberName, sourceFilePath, sourceLineNumber, mat, mat2, mat3);
         func(mat, mat2, mat3);
     }
 
-    public static void LogCall(this IEnumerable<Mat> mats, Mat mat, Action<IEnumerable<Mat>, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    public void LogCall(this IEnumerable<Mat> mats, Mat mat, Action<IEnumerable<Mat>, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
     {
         LogCall(memberName, sourceFilePath, sourceLineNumber, [.. mats, mat]);
         func(mats, mat);
     }
 
-    public static void LogCall(this Mat mat, Mat mat2, Mat mat3, Mat mat4, Action<Mat, Mat, Mat, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+    public void LogCall(IMat mat2, IMat mat3, IMat mat4, Action<Mat, Mat, Mat, Mat> func, [CallerMemberName] string memberName = "", [CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
     {
         LogCall(memberName, sourceFilePath, sourceLineNumber, mat, mat2, mat3, mat4);
-        func(mat, mat2, mat3, mat4);
+        func(_mat, ((ManagedMat)mat2)._mat, ((ManagedMat)mat3)._mat, ((ManagedMat)mat4)._mat);
     }
 
+
+}
+public interface IMat : IDisposable
+{
+
+}
+internal static class MatExtensions
+{
+    public static IMat ToManagedMat(this Mat mat)
+    {
+        return new ManagedMat(mat);
+    }
     public static byte[] BytesFromMat(this Mat mat)
     {
         var tempFile = Path.Combine(Directory.CreateTempSubdirectory().FullName, "temp.png");
