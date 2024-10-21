18a123921e18ccd4b7fe41f4ac4bc610a529decb
Machriam <no@mail.com>
2024-10-21 16:25:41 +0200

    periodically seen devices are written back to the database

--- GatewayApp/Backend/Plantmonitor.DataModel/DataModel/ConfigurationDatum.extensions.cs
+++ GatewayApp/Backend/Plantmonitor.DataModel/DataModel/ConfigurationDatum.extensions.cs
@@ -11,6 +11,20 @@ public enum ConfigurationDatumKeys
 
 public static class ConfigurationDatumExtensions
 {
+    public static void SetValue(this IQueryable<ConfigurationDatum> data, ConfigurationDatumKeys key, string json)
+    {
+        var keyName = Enum.GetName(key) ?? throw new Exception("Enum key cannot be empty");
+        var existingValue = data.FirstOrDefault(d => d.Key == keyName);
+        if (existingValue == default)
+        {
+            data.Add(new ConfigurationDatum { Key = keyName, Value = json });
+        }
+        else
+        {
+            existingValue.Value = json;
+        }
+    }
+
     public static string GetValue(this IQueryable<ConfigurationDatum> data, ConfigurationDatumKeys key)
     {
         return data.FirstOrDefault(d => d.Key == Enum.GetName(key))?.Value ?? "";
--- GatewayApp/Backend/Plantmonitor.Server/Features/DeviceConfiguration/DeviceConnectionEventBus.cs
+++ GatewayApp/Backend/Plantmonitor.Server/Features/DeviceConfiguration/DeviceConnectionEventBus.cs
@@ -15,6 +15,7 @@ public interface IDeviceConnectionEventBus
 public class DeviceConnectionEventBus(IServiceScopeFactory scopeFactory) : IDeviceConnectionEventBus
 {
     private static List<DeviceHealthState> s_deviceHealths = [];
+    private readonly Timer _updateStoredDevices = new(_ => UpdateStoredDevices(scopeFactory), null, TimeSpan.FromSeconds(10), TimeSpan.FromMinutes(1));
 
     public void UpdateDeviceHealths(IEnumerable<DeviceHealthState> healths)
     {
@@ -31,17 +32,36 @@ public class DeviceConnectionEventBus(IServiceScopeFactory scopeFactory) : IDevi
 
     public IEnumerable<DeviceHealthState> GetAllSeenDevices()
     {
-        using var scope = scopeFactory.CreateScope();
-        var dataContext = scope.ServiceProvider.GetRequiredService<IDataContext>();
-        var storedDevices = dataContext.ConfigurationData.GetValue(ConfigurationDatumKeys.AllSeenDevices);
+        var storedDevices = GetStoredDevices(scopeFactory);
         var onlineDevices = GetDeviceHealthInformation();
         foreach (var device in onlineDevices) yield return device;
-        if (storedDevices.IsEmpty()) yield break;
-        var knownDevicesById = storedDevices.FromJson<List<DeviceHealthState>>() ?? [];
         var onlineDeviceIds = onlineDevices.Select(pd => pd.Health.DeviceId).ToHashSet();
-        foreach (var device in knownDevicesById.Where(kd => !onlineDeviceIds.Contains(kd.Health.DeviceId)))
+        foreach (var device in storedDevices.Where(kd => !onlineDeviceIds.Contains(kd.Health.DeviceId)))
         {
             yield return new DeviceHealthState(device.Health, 999, "");
         }
+    }
+
+    private static void UpdateStoredDevices(IServiceScopeFactory scopeFactory)
+    {
+        var storedDevices = GetStoredDevices(scopeFactory);
+        var onlineIds = s_deviceHealths.Select(dh => dh.Health.DeviceId).ToHashSet();
+        var newJson = storedDevices
+            .Where(sd => !onlineIds.Contains(sd.Health.DeviceId))
+            .Pipe(sd => Enumerable.Concat(sd, s_deviceHealths))
+            .AsJson();
+        using var scope = scopeFactory.CreateScope();
+        using var dataContext = scope.ServiceProvider.GetRequiredService<IDataContext>();
+        dataContext.ConfigurationData.SetValue(ConfigurationDatumKeys.AllSeenDevices, newJson);
+        dataContext.SaveChanges();
+    }
+
+    private static List<DeviceHealthState> GetStoredDevices(IServiceScopeFactory scopeFactory)
+    {
+        using var scope = scopeFactory.CreateScope();
+        using var dataContext = scope.ServiceProvider.GetRequiredService<IDataContext>();
+        var storedDevices = dataContext.ConfigurationData.GetValue(ConfigurationDatumKeys.AllSeenDevices);
+        if (storedDevices.IsEmpty()) return [];
+        return storedDevices.FromJson<List<DeviceHealthState>>() ?? [];
     }
 }
