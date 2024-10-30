8dfcdd61acb4ed72755fb622c1131ffb7fa3775f
Machriam <no@mail.com>
2024-10-30 14:51:26 +0100

    Upon initial installation, check if currentPosition does not exist. If not create the file with position 0

--- PlantMonitorControl/Install/install.sh
+++ PlantMonitorControl/Install/install.sh
@@ -60,4 +60,6 @@ sudo sed -i 's/CONF_SWAPSIZE=100/CONF_SWAPSIZE=1024/g' /etc/dphys-swapfile
 sudo dphys-swapfile setup
 sudo dphys-swapfile swapon
 
+sudo bash -c 'if [ ! -f /root/currentPosition.txt ]; then echo 0 | tee /root/currentPosition.txt; fi'
+
 sudo reboot now
